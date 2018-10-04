d6f26c27c91006dd31480414b20705675ef15a99
Matt-Payne <matthew.payne.15@cnu.edu>
2018-10-04 00:01:19 -0400

    first part working

--- MIPS1a_Payne.asm
+++ MIPS1a_Payne.asm
@@ -1,27 +1,28 @@
 .text
 
-.global main
+.globl main
 
 main:
-la $t0, 268501024 # load first base address
+la $t0, 268501024 # load first base address 10010020
 li $t1, 64 # load immediate 100 into register t1
 
-sw $t1, 32($t0)
+sw $t1, 8($t0)
 
-la $t0, 268501056
-li $t1, 00000000d
 
-sw $t1, 16($t0)
+la $t0, 268501056 #10010040
+li $t1, 100
 
-la $t0, 268501056
-li $t1, 00000000f
+sw $t1, 4($t0)
 
-sw $t1, 16($t0)
+la $t0, 268501056 #10010040
+li $t1, 101
 
-la $t0, 268501088
-li $t1, fffffffff
+sw $t1, 12($t0)
 
-sw $t1, 64($t0)
+la $t0, 268501088 #10010060
+li $t1, 102
+
+sw $t1, 4($t0)
 
 
 
