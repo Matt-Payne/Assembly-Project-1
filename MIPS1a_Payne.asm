c8560c4be88140bb11019bf2bc218aada1f31f4a
Matt-Payne <matthew.payne.15@cnu.edu>
2018-10-04 12:06:15 -0400

    finished

--- MIPS1a_Payne.asm
+++ MIPS1a_Payne.asm
@@ -4,23 +4,23 @@
 
 main:
 la $t0, 268501024 # load first base address 10010020
-li $t1, 64 # load immediate 100 into register t1
+li $t1, 100 # load immediate 100 into register t1
 
 sw $t1, 8($t0)
 
 
 la $t0, 268501056 #10010040
-li $t1, 100
+li $t1, 13
 
 sw $t1, 4($t0)
 
 la $t0, 268501056 #10010040
-li $t1, 101
+li $t1, 15
 
-sw $t1, 12($t0)
+sw $t1, 20($t0)
 
 la $t0, 268501088 #10010060
-li $t1, 102
+li $t1, -1
 
 sw $t1, 4($t0)
 
