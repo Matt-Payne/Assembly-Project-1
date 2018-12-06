6f054884cb3d5aa828db122eb9e7cb9d7d3aa54e
Matt-Payne <matthew.payne.15@cnu.edu>
2018-12-06 11:38:59 -0500

    branch flags + strings

--- mips4_Payne.asm
+++ mips4_Payne.asm
@@ -41,7 +41,7 @@ mul.s $f3,$f0,$f9
 mov.s $f12,$f3
 li $v0, 2
 syscall
-
+j end
 
 conC: #from C to F
 l.s $f1, c1 # Load Floating Point Single conversion factor 1
@@ -51,6 +51,29 @@ add.s $f3, $f3, $f1 # F = 32 + C*180/100
 mov.s $f12, $f3 # Copy $f3 to $f12
 li $v0, 2 # Load 2 = print_float into $v0
 syscall # print value in $f12
+
+end:
+l.s $f31,coldF
+l.s $f30,hotF
+c.lt.s $f0,$f31
+bc1t printCold
+
+c.lt.s $f30,$f0
+bc1t printHot
+
+printCold:
+la $a0,coldstr # grab floating number
+li $v0,4
+syscall
+j finish
+
+printHot:
+la $a0, hotstr
+li $v0,4
+syscall
+
+finish:
+li $t8,0
 
 
 .data
@@ -65,6 +88,5 @@ c2: .float 1.8 # conversion factor 2
 coldF: .float 0.0 # cutoff for "cold" temperature
 oneH: .float 100.0
 oneE: .float 180.0
-
 
 
