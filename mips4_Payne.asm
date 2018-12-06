2215509083ab589cea59a5fac3733cefa209ae7c
Matt-Payne <matthew.payne.15@cnu.edu>
2018-12-06 13:09:45 -0500

    conversion update+ loop added

--- mips4_Payne.asm
+++ mips4_Payne.asm
@@ -30,7 +30,7 @@ beq $t0,$t6,conC
 j convF
 
 
-convF:
+convF: #from F to C
 l.s $f1,c1
 l.s $f2, c2
 l.s $f11,oneH
@@ -41,7 +41,16 @@ mul.s $f3,$f0,$f9
 mov.s $f12,$f3
 li $v0, 2
 syscall
-j end
+l.s $f31, coldF
+l.s $f30,hotF
+c.lt.s $f4,$f31
+bc1t printCold
+
+c.lt.s $f30,$f4
+bc1t printHot
+
+
+j finish
 
 conC: #from C to F
 l.s $f1, c1 # Load Floating Point Single conversion factor 1
@@ -51,15 +60,17 @@ add.s $f3, $f3, $f1 # F = 32 + C*180/100
 mov.s $f12, $f3 # Copy $f3 to $f12
 li $v0, 2 # Load 2 = print_float into $v0
 syscall # print value in $f12
-
-end:
-l.s $f31,coldF
+l.s $f31, coldF
 l.s $f30,hotF
-c.lt.s $f0,$f31
+c.lt.s $f3,$f31
 bc1t printCold
 
-c.lt.s $f30,$f0
+c.lt.s $f30,$f3
 bc1t printHot
+
+j finish
+
+
 
 printCold:
 la $a0,coldstr # grab floating number
@@ -73,7 +84,14 @@ li $v0,4
 syscall
 
 finish:
-li $t8,0
+li $t7,121 # y to compare
+li $t6,110 # b to compare
+la $a0,redostr #ask for another run
+li $v0,4
+syscall
+
+li $v0,12 #reads in char, 97 for a 98 for b
+syscall #print
 
 
 .data
@@ -81,12 +99,14 @@ askstr1: .asciiz "a. Convert Fahrenheit to Celsius, or b. Convert Celsius to Fah
 askstr2: .asciiz "Enter a floating temperature "
 coldstr: .asciiz "\nThat is COLD "
 hotstr: .asciiz "\nThat is HOT "
-hotF: .float 120
+redostr .asciiz "\n Do you like to do another conversion? Y for yes N for no"
+hotF: .float 119
 newline: .asciiz "\n"
 c1: .float 32 # conversion factor 1
 c2: .float 1.8 # conversion factor 2
-coldF: .float 0.0 # cutoff for "cold" temperature
+coldF: .float 1.0 # cutoff for "cold" temperature
 oneH: .float 100.0
 oneE: .float 180.0
-
+# y is 121
+# n is 110
 
